021167b9ccc146b09096e65e73e9182a65e078f1
unknown <mwgoodwin2000@gmail.com>
2023-03-09 08:22:29 -0700

    finishing touches

--- react-portfolio/src/component/Contact/index.js
+++ react-portfolio/src/component/Contact/index.js
@@ -1,43 +1,31 @@
-import emailjs from "emailjs-com";
-import React from 'react';
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+import './contact.scss'
 
-export default function ContactUs() {
+const Contact = () => {
+  const form = useRef();
 
-    function sendEmail(e) {
-        e.preventDefault();
+  const sendEmail = (e) => {
+    e.preventDefault();
 
-    emailjs.sendForm('gmail', 'youtube_template', e.target, 'user_JABO21I8Gm6sxByJH17Nu')
-        .then((result) => {
-            console.log(result.text);
-        }, (error) => {
-            console.log(error.text);
-        });
-        e.target.reset()
-    }
-
-    return(
-        <div>
-            <div className="container">
-            <form onSubmit={sendEmail}>
-                    <div className="row pt-5 mx-auto">
-                        <div className="col-8 form-group mx-auto">
-                            <input type="text" className="form-control" placeholder="Name" name="name"/>
-                        </div>
-                        <div className="col-8 form-group pt-2 mx-auto">
-                            <input type="email" className="form-control" placeholder="Email Address" name="email"/>
-                        </div>
-                        <div className="col-8 form-group pt-2 mx-auto">
-                            <input type="text" className="form-control" placeholder="Subject" name="subject"/>
-                        </div>
-                        <div className="col-8 form-group pt-2 mx-auto">
-                            <textarea className="form-control" id="" cols="30" rows="8" placeholder="Your message" name="message"></textarea>
-                        </div>
-                        <div className="col-8 pt-3 mx-auto">
-                            <input type="submit" className="btn btn-info" value="Send Message"></input>
-                        </div>
-                    </div>
-                </form>
-            </div>
-        </div>
-    )
+    emailjs.sendForm('service_165gyle', 'template_eq6t1kt', form.current, 'yxOjDlL4Sl8IE0m54')
+    e.target.reset()
+  };
+  return (
+    <section id='contact'>
+      <div className='contact-header'>
+        <h2>CONTACT ME</h2>
+      </div>
+      <div className='contact-container'>
+        <form ref={form} onSubmit={sendEmail}>
+          <input type='text' name='subject' placeholder='Subject' required></input>
+          <input type='text' name='name' placeholder='Your Full Name' required></input>
+          <input type='email' name='from_name' placeholder='Your Email' required></input>
+          <textarea name='message' rows='7' placeholder='Your Message' required></textarea>
+          <button type='submit' className='btn'>SUBMIT</button>
+        </form>
+      </div>
+    </section>
+  )
 }
+export default Contact 
