a3ec31d30367a65116ea380661ed5513c267ce09
unknown <mwgoodwin2000@gmail.com>
2023-03-09 06:23:12 -0700

    updating react

--- react-portfolio/src/component/Contact/index.js
+++ react-portfolio/src/component/Contact/index.js
@@ -1,45 +1,43 @@
-import './index.scss'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {
-    faRobot,
-    faScroll,
-    faL
-  } from '@fortawesome/free-solid-svg-icons'
+import emailjs from "emailjs-com";
+import React from 'react';
 
-const Contact = () => {
-  return (
-    <div className="container about">
-      <div className="text">
-        <h1>
-         Contact Me!
-        </h1>
-        <h2>
-          These are the best ways to contact me:
-        </h2>
-        <ul>
-          Click the icons to redirect to the respected website.
-          <li>
-            Github:
-        <a href="https://github.com/marcusgoodwin">
-        <FontAwesomeIcon icon={faRobot} color="rgb(151, 233, 207)" />
-      </a>
-          </li>
-          <li>
-            Email:
-        <a href="mailto:mwgoodwin2000@gmail.com">
-        <FontAwesomeIcon icon={faScroll} color="rgb(151, 233, 207)" />
-        </a>
-          </li>
-          <li>
-            LinkedIn:
-        <a href="https://www.linkedin.com/in/marcus-goodwin-b745a2269/">
-        <FontAwesomeIcon icon={faL} color="rgb(151, 233, 207)" />
-        </a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  )
+export default function ContactUs() {
+
+    function sendEmail(e) {
+        e.preventDefault();
+
+    emailjs.sendForm('gmail', 'youtube_template', e.target, 'user_JABO21I8Gm6sxByJH17Nu')
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
+        e.target.reset()
+    }
+
+    return(
+        <div>
+            <div className="container">
+            <form onSubmit={sendEmail}>
+                    <div className="row pt-5 mx-auto">
+                        <div className="col-8 form-group mx-auto">
+                            <input type="text" className="form-control" placeholder="Name" name="name"/>
+                        </div>
+                        <div className="col-8 form-group pt-2 mx-auto">
+                            <input type="email" className="form-control" placeholder="Email Address" name="email"/>
+                        </div>
+                        <div className="col-8 form-group pt-2 mx-auto">
+                            <input type="text" className="form-control" placeholder="Subject" name="subject"/>
+                        </div>
+                        <div className="col-8 form-group pt-2 mx-auto">
+                            <textarea className="form-control" id="" cols="30" rows="8" placeholder="Your message" name="message"></textarea>
+                        </div>
+                        <div className="col-8 pt-3 mx-auto">
+                            <input type="submit" className="btn btn-info" value="Send Message"></input>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
 }
-
-export default Contact;
