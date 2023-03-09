d8b3426406c85cf5320436e92510df6497b73ee9
unknown <mwgoodwin2000@gmail.com>
2023-03-09 09:28:30 -0700

    reorganizing

--- src/component/SideBar/index.js
+++ src/component/SideBar/index.js
@@ -2,7 +2,6 @@ import './index.scss'
 import {  NavLink } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
-  faHouse,
   faClipboard,
   faHandshake,
   faAddressCard,
@@ -17,24 +16,24 @@ const SideBar = () => (
   <div className="nav">
   <h2>Full-Stack Developer</h2>
     <div className="logo">
-    <NavLink exact="true" to="/">
+    <NavLink exact="true" to="/react-portfolio">
         <FontAwesomeIcon icon={faM} color="rgb(151, 233, 207)" />
       </NavLink>
-      <NavLink exact="true" to="/">
+      <NavLink exact="true" to="/react-portfolio">
         <FontAwesomeIcon icon={faG} color="rgb(151, 233, 207)" />
       </NavLink>
     </div>
     <nav>
-      <NavLink exact="true" to="/">
+      <NavLink exact="true" to="/react-portfolio">
         <FontAwesomeIcon icon={faAddressCard} color="rgb(151, 233, 207)" />
       </NavLink>
-      <NavLink exact="true" to="/projects">
+      <NavLink exact="true" to="/react-portfolio/projects">
         <FontAwesomeIcon icon={faClipboard} color="rgb(151, 233, 207)" />
       </NavLink>
-      <NavLink exact="true" to="/contact">
+      <NavLink exact="true" to="/react-portfolio/contact">
         <FontAwesomeIcon icon={faHandshake} color="rgb(151, 233, 207)" />
       </NavLink>
-      <NavLink exact="true" to="/resume">
+      <NavLink exact="true" to="/react-portfolio/resume">
         <FontAwesomeIcon icon={faFile} color="rgb(151, 233, 207)" />
       </NavLink>
     </nav>
