e13bfa879378536f6e95c04d5163144657c5670e
unknown <mwgoodwin2000@gmail.com>
2023-03-10 11:44:57 -0700

    adding some links for contact me page

--- src/component/Projects/index.js
+++ src/component/Projects/index.js
@@ -1,7 +1,7 @@
 import './index.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 // import Bg from '../../assets/images/night.gif'
-import {faQuestion, faPencil} from '@fortawesome/free-solid-svg-icons'
+import {faQuestion, faPencil, faDollarSign, faDollar} from '@fortawesome/free-solid-svg-icons'
 
 const Projects = () => {
     return (
@@ -15,8 +15,13 @@ const Projects = () => {
       </a>
                </h2>
                <h2>Promptlio
-               <a href="https://github.com/lbako801/Promptlio">
+               <a href="https://promptlio.herokuapp.com/">
         <FontAwesomeIcon icon={faPencil} color="rgb(151, 233, 207)" />
+      </a>
+               </h2>
+               <h2>CashFlow
+               <a href="https://cashflow.herokuapp.com/">
+        <FontAwesomeIcon icon={faDollar} color="rgb(151, 233, 207)" />
       </a>
                </h2>
                <div className="product">
