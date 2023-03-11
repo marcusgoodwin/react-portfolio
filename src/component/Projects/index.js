6b643703e2915d814c6d3ddd75e0947ce55dc4cd
unknown <mwgoodwin2000@gmail.com>
2023-03-10 19:42:56 -0700

    fixing page

--- src/component/Projects/index.js
+++ src/component/Projects/index.js
@@ -1,35 +1,41 @@
 import './index.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 // import Bg from '../../assets/images/night.gif'
-import {faQuestion, faPencil, faDollarSign, faDollar} from '@fortawesome/free-solid-svg-icons'
+import {
+  faQuestion,
+  faPencil,
+  faDollar,
+} from '@fortawesome/free-solid-svg-icons'
 
 const Projects = () => {
-    return (
-        <body>
-        <div className="container projects">
-            <div className="text">
-               <h1>Welcome to my Projects Page!</h1>
-               <h2>PokeQuiz
-               <a href="https://github.com/Jacob-Aston/buuzz-quiz">
-        <FontAwesomeIcon icon={faQuestion} color="rgb(151, 233, 207)" />
-      </a>
-               </h2>
-               <h2>Promptlio
-               <a href="https://promptlio.herokuapp.com/">
-        <FontAwesomeIcon icon={faPencil} color="rgb(151, 233, 207)" />
-      </a>
-               </h2>
-               <h2>CashFlow
-               <a href="https://cashflow.herokuapp.com/">
-        <FontAwesomeIcon icon={faDollar} color="rgb(151, 233, 207)" />
-      </a>
-               </h2>
-               <div className="product">
-               </div>
-            </div>
+  return (
+    <body>
+      <div className="container projects">
+        <div className="text">
+          <h1>Welcome to my Projects Page!</h1>
+          <h2>
+            PokeQuiz
+            <a href="https://github.com/Jacob-Aston/buuzz-quiz">
+              <FontAwesomeIcon icon={faQuestion} color="rgb(151, 233, 207)" />
+            </a>
+          </h2>
+          <h2>
+            Promptlio
+            <a href="https://promptlio.herokuapp.com/">
+              <FontAwesomeIcon icon={faPencil} color="rgb(151, 233, 207)" />
+            </a>
+          </h2>
+          <h2>
+            CashFlow
+            <a href="https://cashflow.herokuapp.com/">
+              <FontAwesomeIcon icon={faDollar} color="rgb(151, 233, 207)" />
+            </a>
+          </h2>
+          <div className="product"></div>
         </div>
-        </body>
-    )
+      </div>
+    </body>
+  )
 }
 
-export default Projects;
+export default Projects
